b3883375ada419b32f8e1aefec18ae776c8eda70
Brandon Sprague <brandon00sprague@gmail.com>
2013-07-14 23:58:15 -0400

    Initial commit, getting things started.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sexysix::Application.config.secret_key_base = '1365a4199ae12ae564a23be8ba8050e52fcc5032bb3f20f86519f82cd22106786f65ed3f9c23c4219af22dbd14496bef0c4c9e07ecbcf9537e492d9bed25976d'
